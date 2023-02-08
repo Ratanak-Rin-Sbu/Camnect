cca73fa49f7788e36bcad4c7f15c7bf22bb21d9d
Ratanak-Rin-Sbu <100118148+Ratanak-Rin-Sbu@users.noreply.github.com>
2023-02-08 21:03:28 +0900

    Implemented profile, friend list, advert, and posts.

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -32,9 +32,9 @@ export const authSlice = createSlice({
     setPosts: (state, action) => {
       state.posts = action.payload.posts;
     },
-    setPosts: (state, action) => {
+    setPost: (state, action) => {
       const updatedPosts = state.posts.map((post) => {
-        if (post._id === action.payload.post_id) return action.payload.post;
+        if (post._id === action.payload.post._id) return action.payload.post;
         return post;
       });
       state.posts = updatedPosts;
--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,46 +1,45 @@
 import User from "../models/User.js";
 
-// READ
+/* READ */
 export const getUser = async (req, res) => {
   try {
     const { id } = req.params;
     const user = await User.findById(id);
     res.status(200).json(user);
-  } catch (error) {
-    res.status(404).json({ error: error.message });
+  } catch (err) {
+    res.status(404).json({ message: err.message });
   }
-}
+};
 
 export const getUserFriends = async (req, res) => {
   try {
     const { id } = req.params;
     const user = await User.findById(id);
 
-    const friends = await Promise.all(user.friends.map((id) => User.findById(id)));
+    const friends = await Promise.all(
+      user.friends.map((id) => User.findById(id))
+    );
     const formattedFriends = friends.map(
-      ({ _id, fistName, lastName, occupation, location, picturePath }) => {
-        return { _id, fistName, lastName, occupation, location, picturePath };
-    });
+      ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+        return { _id, firstName, lastName, occupation, location, picturePath };
+      }
+    );
     res.status(200).json(formattedFriends);
-  } catch (error) {
-    res.status(404).json({ error: error.message });
+  } catch (err) {
+    res.status(404).json({ message: err.message });
   }
-}
+};
 
-// UPDATE
+/* UPDATE */
 export const addRemoveFriend = async (req, res) => {
   try {
     const { id, friendId } = req.params;
     const user = await User.findById(id);
     const friend = await User.findById(friendId);
 
-    // if user's friend_list consists of that friend's id => remove it from user's friend_list and remove that user's id
-    // from that friend's friend_list
     if (user.friends.includes(friendId)) {
       user.friends = user.friends.filter((id) => id !== friendId);
       friend.friends = friend.friends.filter((id) => id !== id);
-    // if not, it means we're trying to use the addFriend function. Include that friend to user's friend_list and include that
-    // user to that friend's friend_list
     } else {
       user.friends.push(friendId);
       friend.friends.push(id);
@@ -48,13 +47,17 @@ export const addRemoveFriend = async (req, res) => {
     await user.save();
     await friend.save();
 
-    const friends = await Promise.all(user.friends.map((id) => User.findById(id)));
+    const friends = await Promise.all(
+      user.friends.map((id) => User.findById(id))
+    );
     const formattedFriends = friends.map(
-      ({ _id, fistName, lastName, occupation, location, picturePath }) => {
-        return { _id, fistName, lastName, occupation, location, picturePath };
-    });
+      ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+        return { _id, firstName, lastName, occupation, location, picturePath };
+      }
+    );
+
     res.status(200).json(formattedFriends);
-  } catch (error) {
-    res.status(404).json({ error: error.message });
+  } catch (err) {
+    res.status(404).json({ message: err.message });
   }
-}
+};
